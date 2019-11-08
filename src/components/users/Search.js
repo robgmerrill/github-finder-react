2044d7cfcc3c3132dbed7c093a059adb94405753
Robert Merrill <robertmerrill@Roberts-MacBook-Air.local>
2019-11-08 15:13:00 -0800

    Form validation and Alert component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,16 +9,20 @@ export class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
   };
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please Enter Something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   render() {
