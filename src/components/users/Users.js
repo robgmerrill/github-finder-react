b15b1072c19b76c1205859a7c8b22f77a37d0f00
Robert Merrill <robertmerrill@Roberts-MacBook-Air.local>
2019-11-08 07:56:18 -0800

    Add spinner component and convert Users component to functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-    render() {
+
+const Users = ({users, loading}) => {
+    if (loading) {
+        return <Spinner />
+    } else {
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => (
+                {users.map(user => (
                      <UserItem 
                         key={user.id}
                         user={user}
@@ -14,6 +19,11 @@ class Users extends Component {
             </div>
         )
     }
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
 }
 
 const userStyle = {
