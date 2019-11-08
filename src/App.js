7037e55d27c153fdb2baa88d1d882df022c2942b
Robert Merrill <robertmerrill@Roberts-MacBook-Air.local>
2019-11-07 17:04:10 -0800

    Http requests with axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios'
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  // componentDidMount() {
+  //   axios.get('https://api.github.com/users').then(res => console.log(res.data))
+  // }
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data);
+    this.setState({users: res.data, loading: false})
+    console.log(res.data);
+  }
+
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,27 +2,10 @@ import React, { Component } from 'react'
 import UserItem from './UserItem'
 
 class Users extends Component {
-    state = {
-        users: [
-            {id: '1',
-            login: 'octocat', 
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/octocat'}, 
-            {id: '2',
-            login: 'horse', 
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/octocat'},
-            {id: '3',
-            login: 'cat', 
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/octocat'}
-        ]
-    }
-
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                      <UserItem 
                         key={user.id}
                         user={user}
