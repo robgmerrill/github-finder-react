ec21722ccf3017815a42b39320ae5d96dea9655c
Robert Merrill <robertmerrill@Roberts-MacBook-Air.local>
2019-11-11 11:35:44 -0800

    User component UI and layout

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import User from './components/users/User';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -39,6 +41,21 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Get single github user
+  getUser = async username => {
+    console.log('username', username);
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      //   `https://api.github.com/users/${username}?client_id=
+      //  ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      //  ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    console.log('get user data', res.data);
+    this.setState({ user: res.data, loading: false });
+    console.log(this.state.user);
+  };
+
   // Clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -51,7 +68,7 @@ class App extends Component {
     }, 5000);
   };
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
 
     return (
       <Router>
@@ -76,6 +93,18 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const UserItem = ({user: {login, avatar_url, html_url}} ) => {
-
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          className="round-image"
-          style={{ width: '60px' }}
-          alt="Programmer"
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        className='round-image'
+        style={{ width: '60px' }}
+        alt='Programmer'
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 UserItem.propTypes = {
-    user: PropTypes.object.isRequired,
-}
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
